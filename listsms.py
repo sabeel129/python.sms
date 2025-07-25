675bfbd780fcacd4afb02745b0da829afb852a51
sabeel129 <sheikhmohammedsabeel@gmail.com>
2025-07-25 14:05:05 +0530

    Add files via upload

---
+++ listsms.py
@@ -0,0 +1,9 @@
+numbers = [5, 12, 23, 34, 45]
+print("First element:", numbers[0])
+print("Last element:", numbers[-1])
+print("All elements in the list:")
+for num in numbers:
+    print(num)
+print("List in reverse order:")
+for num in reversed(numbers):
+    print(num)
